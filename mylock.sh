9df61d0b1626d69b5f723afa7af6b220bd0859f7
phischdev <phisch@oulook.de>
2017-03-26 14:35:05 +0200

    systemctl instead of pm-

--- mylock.sh
+++ mylock.sh
@@ -3,4 +3,4 @@ scrot /tmp/screen_locked.png
 convert /tmp/screen_locked.png -scale 10% -scale 1000% /tmp/screen_locked.png
 i3lock -i /tmp/screen_locked.png
 rm /tmp/screen_locked.png
-sleep 60; pgrep i3lock && sudo pm-suspend
+sleep 60; pgrep i3lock && systemctl suspend
