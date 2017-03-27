2b1e13b94bff1c77b9d87eed7a07d19c714fa52b
phischdev <phisch@oulook.de>
2017-03-27 02:04:16 +0200

    minor

--- mylock.sh
+++ mylock.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
 scrot /tmp/screen_locked.png
 convert /tmp/screen_locked.png -scale 10% -scale 1000% /tmp/screen_locked.png
-i3lock -i /tmp/screen_locked.png
+i3lock -i /tmp/screen_locked.png &
+sleep 1;
 rm /tmp/screen_locked.png
-sleep 60; pgrep i3lock && systemctl suspend
+#sleep 60; pgrep i3lock && systemctl suspend
