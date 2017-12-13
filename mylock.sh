40bcd482ed7bdc548222c09bfee19ef386abd8a4
phischdev <phisch@oulook.de>
2017-12-13 13:05:28 +0100

    lock.service

--- mylock.sh
+++ mylock.sh
@@ -4,4 +4,4 @@ convert /tmp/screen_locked.png -scale 10% -scale 1000% /tmp/screen_locked.png
 i3lock -i /tmp/screen_locked.png &
 sleep 1;
 rm /tmp/screen_locked.png
-#sleep 60; pgrep i3lock && systemctl suspend
+# sleep 6; pgrep i3lock && systemctl suspend
