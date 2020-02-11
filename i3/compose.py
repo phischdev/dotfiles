972ec7f9fbd889d436b8a31dc57225d475e32a80
phischdev <phischdev@users.noreply.github.com>
2020-02-11 03:42:50 +0100

    implemented conditionals: code is not tested, has errors

--- i3/compose.py
+++ i3/compose.py
@@ -4,38 +4,111 @@ import os
 import argparse
 import sys
 
-#parser = argparse.ArgumentParser(description='compose files with <include file>')
-#parser.add_argument('-o', action="store", dest="output")
-#args = parser.parse_args()
+parser = argparse.ArgumentParser(
+    description='i3 config preprocessor. Include files with <include file>.\nEvaluate partially with <if FLAG><else><endif>.\nSet Flags as arguments or with <set FLAG true|false>.')
 
+parser.add_argument("base", help="the base file for composing the config")
+parser.add_argument("-o", "--out", default="compose-config",
+                    help="name of output file. Default: 'compose-config'.")
+parser.add_argument("--flags", nargs='+',
+                    help="flags to be set 'true' during evaluation.")
+args = parser.parse_args()
 
+# flags
+flags = {"true": True}
+for flag in args.flags:
+    flags[flag] = True
+flags["false"] = False
 
-folder = os.path.dirname(os.path.abspath(__file__))
+# parser commands
+strInclude = "<include "
+strIf = "<if "
+strElse = "<else>"
+strEndIf = "<endif>"
+strSet = "<set "
 
-input = os.path.join(folder, "compose-config")
-output = os.path.join(folder, "config")
+# files
+# folder = os.path.dirname(os.path.abspath(__file__))
+folder = os.path.dirname(os.path.abspath(args.base))
 
-search = "<include "
+baseFile = args.base #os.path.join(folder, args.base)
+output = os.path.join(folder, args.out)
+
 
 # resulting config file (output)
 print("writing to", output)
 with open(output, "w") as config:
 
-    # compose file (input)
-    print("reading from", input)
-    with open(input, "r") as compose:
+    # startig with base file
+    readStack = ["<include "+baseFile+">"]
+    ifStack = []
+    ifVal = True  # determines if current block is evaluated
 
-        for line in compose:
-                    
-            # including file
-            if line.startswith(search):
-                include = line.strip()[len(search):-1]
-                include = os.path.join(folder, include)
-                print("including", include)
-                with open(include, "r") as include_file:
-                    config.write("# " + include + "\n")
-                    for i_line in include_file:
-                        config.write(i_line)
-            # normal line            
+    for line in readStack:
+
+        # endif
+        if line.startswith(strEndIf):
+            if ifStack.count == 0:
+                print("endif without if")
+                exit(-1)
             else:
-                config.write(line)
+                (flag, ifVal) = ifStack.pop()
+            continue
+
+        # if block
+        elif line.startswith(strIf):
+            flag = line.strip()[len(strIf):-1]
+
+            flagValue = False
+            if flag in flags:
+                flagValue = flags[flag]
+
+            # push flag and current ifVal
+            ifStack.append((flag, ifVal))
+
+            # if in live block evaluate if
+            if ifVal:
+                # dead or live block?
+                ifVal = flagValue
+
+            continue
+
+        # else block
+        elif line.startswith(strElse):
+            if ifStack.count == 0:
+                print("else without if")
+                exit(-1)
+            else:
+                (flag, peekIfVal) = ifStack[-1]
+                # if in live block then evaluate else
+                if peekIfVal:
+                    ifVal = not ifVal
+            continue
+
+        # dead code
+        if not ifVal:
+            continue
+
+        # set flag
+        if line.startswith(strSet):
+            setStatement = line.strip()[len(strSet):-1]
+            setStatement = setStatement.split(" ")
+            if not (setStatement.count == 2 and ["true", "false"].index(setStatement[1]) >= 0):
+                print("invalid set", setStatement)
+                exit(-1)
+            else:
+                flags[setStatement[0]] = setStatement[1]
+
+        # include file
+        elif line.startswith(strInclude):
+            include = line.strip()[len(strInclude):-1]
+            include = os.path.join(folder, include)
+            print("including", include)
+            with open(include, "r") as include_file:
+                readStack.append("# " + include + "\n")
+                for i_line in include_file:
+                    readStack.append(i_line)
+
+        # normal line
+        else:
+            config.write(line)
